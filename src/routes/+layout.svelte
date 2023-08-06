9a1b9494a7b38a5cebe980497bd676e8aec9def4
Sean Swanson <sean-swan@hotmail.com>
2023-08-05 21:01:58 -0700

    Now pulling data from the JSON file to populate the candidates section.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>PoliScope</header>
+<main>
+	<slot />
+</main>
+<footer>Made with ♡ by <a href="seancswanson.com">Sean Swanson</a></footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import candidates from '$lib/data/candidates.json';
+</script>
+
+<h1 class="">Candidates</h1>
+{#each candidates.candidates as candidate}
+	<div class="candidate">
+		<img class="rounded-full" src={`https://placehold.co/50`} alt={candidate.name} />
+		<h2>{candidate.name}</h2>
+	</div>
+{/each}
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
