924939dae726ac1c691dd5463485d675a5fef042
Sean Swanson <seancswan@gmail.com>
2023-11-16 10:57:22 -0800

    Enabled filter. Changed to radio buttons.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,8 @@
 <footer class="text-center mt-10">
 	<p>
-		Contribute to our data: <a href="https://github.com/poliscope-svelte" class="text-blue-600"
-			>GitHub Repository</a
+		Contribute to our data: <a
+			href="https://github.com/seancswanson/poliscope-sveltekit"
+			class="text-blue-600">GitHub Repository</a
 		>
 	</p>
 	<p>Made with ♡ by <a href="seancswanson.com">Sean Swanson</a></p>
--- src/routes/candidates/CandidateList.svelte
+++ src/routes/candidates/CandidateList.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import CandidateCard from './CandidateCard.svelte';
-
 	import candidates from '$lib/data/candidates.json';
-	import { createSearchStore } from '$lib/stores/search';
+	import { fade } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	let filterVisible = false;
 	let selectedParty = 'All';
-	const searchCandidates = candidates.candidates.map((candidate) => ({
-		...candidate,
-		searchTerms: `${candidate.name} ${candidate.party} ${candidate.current_position}`
-	}));
 	const numberCandidates = {
-		total: candidates.candidates.length,
+		all: candidates.candidates.length,
 		democratic: candidates.candidates.filter((candidate) => candidate.party === 'Democratic')
 			.length,
 		republican: candidates.candidates.filter((candidate) => candidate.party === 'Republican')
@@ -18,24 +15,30 @@
 			.length,
 		green: candidates.candidates.filter((candidate) => candidate.party === 'Green').length
 	};
+	let filteredCandidates = candidates.candidates;
+
+	$: {
+		if (selectedParty !== 'All') {
+			filteredCandidates = candidates.candidates.filter(
+				(candidate) => candidate.party === selectedParty
+			);
+		} else {
+			filteredCandidates = candidates.candidates;
+		}
+	}
 	const politicalParties = candidates.candidates
 		.map((candidate) => candidate.party)
 		.filter((value, index, self) => self.indexOf(value) === index);
-	const searchStore = createSearchStore(searchCandidates);
-	import { fade } from 'svelte/transition';
-	import { quintOut } from 'svelte/easing';
-	const latestDataDate = new Date(candidates.latest_data_date);
-	let filterVisible = false;
 </script>
 
-<!-- <div class="filter-bar flex justify-end mb-2">
+<div class="filter-bar flex justify-end mb-2">
 	<button on:click={() => (filterVisible = true)} class={``}
 		><img
 			src="$lib/assets/filter.png"
-			class={`border-black border-2  transition-all ease-in-out shadow-neo w-8 p-1`}
+			class={`border-black border-[1.5px] hover:translate-y-[-2px]  rounded-sm transition-all ease-in-out shadow-neo w-8 p-1`}
 		/></button
 	>
-</div> -->
+</div>
 {#if filterVisible}
 	<div
 		role="button"
@@ -59,34 +62,33 @@
 				Filter Options
 			</div>
 			<div class="content min-w-[250px] mx-auto p-2 flex flex-col gap-2">
-				<div class="party-select">
-					<label for="party" class="block font-medium text-sm">Choose a party:</label>
-					<select
-						id="party"
-						class="w-full border rounded p-2 text-black"
-						bind:value={selectedParty}
-					>
-						<option>All</option>
+				<div class="party-select flex flex-col gap-2">
+					<span class="block font-medium text-sm">Party:</span>
+					<div class="flex flex-col">
 						{#each politicalParties as party}
-							<option>{party}</option>
+							<div class="flex gap-2">
+								<input
+									id={politicalParties.indexOf(party).toString()}
+									type="radio"
+									class="form-radio h-5 w-5 text-gray-600"
+									name="party"
+									value={party}
+									checked={selectedParty === party}
+									on:change={() => {
+										selectedParty = party;
+									}}
+								/>
+								<label for={politicalParties.indexOf(party).toString()} class="">{party}</label>
+							</div>
 						{/each}
-					</select>
-				</div>
-				<div class="search">
-					<label for="search" class="block font-medium text-sm">Search by name:</label>
-					<input
-						id="search"
-						class="w-full p-2 border rounded text-black"
-						type="text"
-						placeholder="Mickey Mouse"
-					/>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 {/if}
 <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
-	{#each $searchStore.data as candidate}
+	{#each filteredCandidates as candidate (candidate.slug)}
 		{@const imgUrl = `/portraits/${candidate.slug}-square.webp`}
 		<CandidateCard {candidate} {imgUrl} />
 	{/each}
