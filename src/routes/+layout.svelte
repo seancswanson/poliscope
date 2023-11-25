53db273786a702b263b3ecbfcc93ddd0fc645402
Sean Swanson <seancswan@gmail.com>
2023-11-25 14:48:38 -0800

    Added OG images in head tag.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { Head } from '$app/env';
 	import Header from '$lib/components/header/Header.svelte';
 	import Footer from '../lib/components/Footer.svelte';
 
@@ -11,6 +12,40 @@
 	export let data;
 	$: pathname = data.pathname;
 </script>
+
+<svelte:head>
+	<title>Poliscope</title>
+	<meta
+		name="description"
+		content="Poliscope is a web application designed to provide clear, unbiased information on candidates and key issues for the 2024 U.S. Presidential Election, helping voters make informed decisions."
+	/>
+
+	<!-- Facebook/OpenGraph Tags -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://poliscope-sveltekit.vercel.app/" />
+	<meta property="og:title" content="Poliscope" />
+	<meta
+		property="og:description"
+		content="Poliscope is a web application designed to provide clear, unbiased information on candidates and key issues for the 2024 U.S. Presidential Election, helping voters make informed decisions."
+	/>
+	<meta
+		property="og:image"
+		content="https://poliscope-sveltekit.vercel.app/android-chrome-192x192.png"
+	/>
+
+	<!-- Twitter Card Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:url" content="https://poliscope-sveltekit.vercel.app/" />
+	<meta name="twitter:title" content="Poliscope" />
+	<meta
+		name="twitter:description"
+		content="Poliscope is a web application designed to provide clear, unbiased information on candidates and key issues for the 2024 U.S. Presidential Election, helping voters make informed decisions."
+	/>
+	<meta
+		name="twitter:image"
+		content="https://poliscope-sveltekit.vercel.app/android-chrome-192x192.png"
+	/>
+</svelte:head>
 
 <div class="page-wrapper min-h-screen h-full grid grid-rows-[auto,auto,1fr,auto]">
 	<Header />
