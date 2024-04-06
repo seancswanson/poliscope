8fa1085751caa7e12a3cce73f42fe52b1d8ee38b
Sean Swanson <seancswan@gmail.com>
2024-04-06 14:33:30 -0700

    Added a types file, broke candidateList into 3 separate components for readability. Refactored various functions that sort and set various properties. Removed unused properties from candidtes.json file.

--- src/routes/candidates/CandidateList.svelte
+++ src/routes/candidates/CandidateList.svelte
@@ -1,147 +1,30 @@
 <script lang="ts">
+	import type { Candidate } from '$lib/types';
+	export let filteredCandidates: Candidate[], status: string, selectedParty: string;
 	import CandidateCard from './CandidateCard.svelte';
-	import candidates from '$lib/data/candidates.json';
-	import { fade } from 'svelte/transition';
-	import { quintOut } from 'svelte/easing';
 
-	const sortedCandidates = sortCandidatesByName(candidates.candidates);
+	$: emptyMessage = getEmptyMessage(selectedParty, status);
 
-	const politicalParties = sortedCandidates
-		.map((candidate) => candidate.party)
-		.filter((value, index, self) => self.indexOf(value) === index);
-
-	const runningCandidates = sortedCandidates.filter((candidate) => candidate.withdrawn === false);
-	const withdrawnCandidates = sortedCandidates.filter((candidate) => candidate.withdrawn === true);
-
-	let filteredCandidates: typeof sortedCandidates;
-	let filterVisible = false;
-	let selectedParty = 'All';
-
-	$: {
-		if (selectedParty !== 'All') {
-			filteredCandidates = sortCandidatesByName(
-				runningCandidates.filter((candidate) => candidate.party === selectedParty)
-			);
-		} else {
-			filteredCandidates = runningCandidates;
+	function getEmptyMessage(party: string, currentStatus: string) {
+		if (party === 'All') {
+			return 'No candidates match this filter.';
 		}
-	}
-
-	function sortCandidatesByName(candidatesList: any[]) {
-		return candidatesList.sort((a, b) => {
-			const aLastName = a.name.split(' ').slice(-1)[0];
-			const bLastName = b.name.split(' ').slice(-1)[0];
-			return aLastName.localeCompare(bLastName);
-		});
+		return currentStatus === 'Currently Running'
+			? `No ${party} party candidates are currently running.`
+			: `No ${party} party candidates have withdrawn.`;
 	}
 </script>
 
-<div class="flex justify-between mb-2 filter-bar">
-	<h2 class="font-bold">Currently Running</h2>
-	<button
-		on:click={() => (filterVisible = true)}
-		on:keydown={() => (filterVisible = true)}
-		class={``}
-		><img
-			alt="filter icon"
-			src="$lib/assets/filter.png"
-			class={`border-black border-[1.5px] hover:translate-y-[-2px]  rounded-sm transition-all ease-in-out shadow-neo w-8 p-1`}
-		/></button
-	>
-</div>
-{#if filterVisible}
-	<div
-		tabindex="0"
-		role="button"
-		aria-labelledby="Title"
-		aria-describedby="Description"
-		transition:fade
-		on:click|stopPropagation
-		on:keydown|stopPropagation
-		class="relative popover"
-	>
-		<div
-			class="backdrop"
-			tabindex="0"
-			role="button"
-			on:click|stopPropagation={() => (filterVisible = false)}
-			on:keydown|stopPropagation={() => (filterVisible = false)}
-			transition:fade={{ delay: 25, duration: 150, easing: quintOut }}
-		/>
-		<div
-			class="absolute top-0 right-0 flex flex-col items-start text-black bg-white border border-black rounded shadow-neo"
-		>
-			<div class="w-full py-1 text-center uppercase bg-gray-300 rounded-t top-bar">
-				Filter Options
-			</div>
-			<div class="content min-w-[250px] mx-auto p-2 flex flex-col gap-2">
-				<div class="flex flex-col gap-2 party-select">
-					<span class="block text-sm font-medium">Party:</span>
-					<div class="flex flex-col">
-						<div class="flex flex-col gap-2">
-							<div class="flex gap-2 party-select">
-								<input
-									id="All"
-									type="radio"
-									class="w-5 h-5 text-gray-600 form-radio"
-									name="party"
-									value="All"
-									checked={selectedParty === 'All'}
-									on:change={() => {
-										selectedParty = 'All';
-									}}
-								/>
-								<label for="All" class="">All</label>
-							</div>
-
-							{#each politicalParties as party}
-								<div class="flex gap-2 party-select">
-									<input
-										id={politicalParties.indexOf(party).toString()}
-										type="radio"
-										class="w-5 h-5 text-gray-600 form-radio"
-										name="party"
-										value={party}
-										checked={selectedParty === party}
-										on:change={() => {
-											selectedParty = party;
-										}}
-									/>
-									<label for={politicalParties.indexOf(party).toString()} class="">{party}</label>
-								</div>
-							{/each}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-{/if}
+<h2 class="mb-2 font-bold">{status}</h2>
 <div class="grid grid-cols-2 gap-4 mb-8 lg:grid-cols-3">
-	{#each filteredCandidates as candidate (candidate.slug)}
-		{@const imgUrl = `/portraits/${candidate.slug}-square.webp`}
-		<CandidateCard {candidate} {imgUrl} />
-	{/each}
+	{#if filteredCandidates.length > 0}
+		{#each filteredCandidates as candidate (candidate.slug)}
+			{@const imgUrl = `/portraits/${candidate.slug}-square.webp`}
+			<CandidateCard {candidate} {imgUrl} />
+		{/each}
+	{:else}
+		<div class="text-center col-span-full">
+			<p>{emptyMessage}</p>
+		</div>
+	{/if}
 </div>
-<h2 class="mb-4 font-bold">Withdrawn</h2>
-<div class="grid grid-cols-2 gap-4 lg:grid-cols-3">
-	{#each withdrawnCandidates as candidate (candidate.slug)}
-		{@const imgUrl = `/portraits/${candidate.slug}-square.webp`}
-		<CandidateCard {candidate} {imgUrl} />
-	{/each}
-</div>
-
-<style>
-	.popover {
-		z-index: 997;
-	}
-
-	.backdrop {
-		width: 100vw;
-		height: 100vh;
-		position: fixed;
-		inset: 0;
-
-		background: rgba(0, 0, 0, 0.7);
-	}
-</style>
